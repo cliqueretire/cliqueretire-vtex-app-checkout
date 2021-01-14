6b9593d46c5a1c3200e1df75d4f4a84b61c0ad75
DanielSuhett <danysuhett@hotmail.com>
2021-01-14 13:11:14 -0300

    Script improve to shipping area

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -1,16 +1,14 @@
-(function () {
+;(function () {
   /* Load Script function we may need to load jQuery from the Google's CDN */
   /* That code is world-reknown. */
   /* One source: http://snipplr.com/view/18756/loadscript/ */
-
   var loadScript = function (url, callback) {
-    var script = document.createElement("script")
-    script.type = "text/javascript"
-
+    var script = document.createElement('script')
+    script.type = 'text/javascript'
     // If the browser is Internet Explorer.
     if (script.readyState) {
       script.onreadystatechange = function () {
-        if (script.readyState == "loaded" || script.readyState == "complete") {
+        if (script.readyState == 'loaded' || script.readyState == 'complete') {
           script.onreadystatechange = null
           callback()
         }
@@ -21,73 +19,83 @@
         callback()
       }
     }
-
     script.src = url
-    document.getElementsByTagName("head")[0].appendChild(script)
+    document.getElementsByTagName('head')[0].appendChild(script)
+  }
+
+  const carrierName = 'Retirar no e-Box Clique Retire'
+  const carrierSelector = "label[id='" + carrierName + "'] > input"
+
+  const logisticInfo = () => {
+    const orderForm = window.vtexjs.checkout.orderForm
+    return orderForm && orderForm.shippingData && orderForm.shippingData.logisticsInfo.length
+      ? orderForm.shippingData.logisticsInfo[0].selectedSla
+      : null
   }
 
   const cr_boxCliqueretire = (orderNo, name) => `
-  <div class="cr_box-cliqueretire">
-    <h4>Receba fora de casa</h4>
-    <div class="cr_box-item col-1-2">
-${
-  !orderNo
-    ? `	<div class="box-item col-1-2 cr_cliqueretire-image">
-    <a href="#" id="cr_buttonOpen" class="btn btn-large btn-success">Escolher local para retirada</a>
-    </div>`
-    : `<p>Você escolheu o e-box ${orderNo} - ${name}</p> 
-  <button id="cr_cleanerLocker">Limpar</button>
-  </div>
-  <div class="box-item col-1-2 cr_cliqueretire-image">
-    <a href="#" id="cr_buttonOpen" class="btn btn-large btn-success">Alterar local para retirada</a>
-  </div>
-  `
-}
-
-    <div class="box-item col-1-2">
-      <div class="content">
-        <div class="inner-content">
-          <p>Você não precisa ter alguem em casa para receber.</p>
-          <p>Utilize um e-Box da Clique Retire para retirar sua compra.</p>
-          <p>
-            <a href="#" id="cr_InfoOpen" class="click-como-funciona"> Como Funciona </a>
-          </p>
+        <div class="cr_box-cliqueretire">
+          <h4>Receba fora de casa</h4>
+          <div class="cr_box-item col-1-2">
+      ${
+        !orderNo
+          ? ` <div class="box-item col-1-2 cr_cliqueretire-image">
+          <a href="#" id="cr_buttonOpen" class="btn btn-large btn-success">Escolher local para retirada</a>
+          </div>`
+          : `<p>Você escolheu o e-box ${orderNo}​ - ${name}​</p> 
+        <button id="cr_cleanerLocker">Limpar</button>
+        </div>
+        <div class="box-item col-1-2 cr_cliqueretire-image">
+          <a href="#" id="cr_buttonOpen" class="btn btn-large btn-success">Alterar local para retirada</a>
+        </div>
+        `
+      }​
+          <div class="box-item col-1-2">
+            <div class="content">
+              <div class="inner-content">
+                <p>Você não precisa ter alguem em casa para receber.</p>
+                <p>Utilize um e-Box da Clique Retire para retirar sua compra.</p>
+                <p>
+                  <a href="#" id="cr_InfoOpen" class="click-como-funciona"> Como Funciona </a>
+                </p>
+                </div>
+              <div class="cr_box-tag">Recomendado</div>
+            </div>
           </div>
-        <div class="cr_box-tag">Recomendado</div>
-      </div>
-    </div>
-    </div>
-    <div class="clearfix"></div>
-  </div>`;
+          </div>
+          <div class="clearfix"></div>
+        </div>`
 
   const cr_dialogTemplate = `<div
-  id="cr_dialog">
-   <div style="display: flex; justify-content: space-between;">
-   <span id="cr_textDialog">Clique Retire</span>
-   <button
-   id="cr_closeIframe">x</button>
-   </div>
-          <iframe
-          id="cr_iframe"
-          frameborder="0"
-          marginheight="0" allowfullscreen></iframe>
-      </div>`;
+        id="cr_dialog">
+         <div style="display: flex; justify-content: space-between;">
+         <span id="cr_textDialog">Clique Retire</span>
+         <button
+         id="cr_closeIframe">x</button>
+         </div>
+                <iframe
+                id="cr_iframe"
+                frameborder="0"
+                marginheight="0" allowfullscreen></iframe>
+            </div>`
 
   const UpdateCliqueRetire = (renderLayout, newOrderNo, newName) => {
-    if (window.location.hash == "#/payment" 
-    || window.location.hash == "#/shipping") $("#cr_trButton").remove()
+    const selectSla = logisticInfo()
 
-    if ($(".shipping-container").length 
-      && !$("#cr_trButton").length) 
-        renderLayout(newOrderNo, newName)
+    if (window.location.hash == '#/payment' || window.location.hash == '#/shipping') $('#cr_trButton').remove()
+    if (
+      $('.shipping-container').length &&
+      !$('#cr_trButton').length &&
+      ($(carrierSelector).is(':checked') === true || selectSla === carrierName)
+    )
+      renderLayout(newOrderNo, newName)
   }
 
   const setItemToStorage = (data) => {
     if (data) {
       const { name, orderNo, userPostalCode, address } = data
-
       window.localStorage.setItem(
-        "cr_selectedLocker",
+        'cr_selectedLocker',
         JSON.stringify({
           name,
           orderNo,
@@ -97,7 +105,7 @@ ${
       )
     } else {
       window.localStorage.setItem(
-        "cr_selectedLocker",
+        'cr_selectedLocker',
         JSON.stringify({
           name: null,
           orderNo: null,
@@ -112,10 +120,13 @@ ${
     return !Address
       ? null
       : {
-          name: Address.complement && Address.complement.startsWith("CR0") ? Address.complement.replace(Address.complement.split(" ")[0], "") : null,
-          orderNo: Address.complement && Address.complement.startsWith("CR0") ? Address.complement.split(" ")[0] : null,
+          name:
+            Address.complement && Address.complement.startsWith('CR0')
+              ? Address.complement.replace(Address.complement.split(' ')[0], '')
+              : null,
+          orderNo: Address.complement && Address.complement.startsWith('CR0') ? Address.complement.split(' ')[0] : null,
           userPostalCode:
-            (Address.complement && !Address.complement.startsWith("CR0")) || !Address.complement
+            (Address.complement && !Address.complement.startsWith('CR0')) || !Address.complement
               ? Address.postalCode
               : locker
               ? locker.userPostalCode
@@ -127,78 +138,105 @@ ${
             state: Address.state,
             street: Address.street,
             complement: Address.complement,
-            number: Address.number || "S/N",
-            country: "BRA",
-            addressType: "residential",
+            number: Address.number || 'S/N',
+            country: 'BRA',
+            addressType: 'residential',
           },
         }
   }
 
   const CRCheckout = async ($) => {
-    let srcModal = ""
-
-    let locker = JSON.parse(window.localStorage.getItem("cr_selectedLocker"))
+    let srcModal = ''
+    let locker = JSON.parse(window.localStorage.getItem('cr_selectedLocker'))
 
     function renderLayout(orderNo, name) {
-      $(".shipping-data").append(`<div id="cr_trButton"></div>`)
-
+      $("label[id='" + carrierName + "']").after(`<div id="cr_trButton"></div>`)
       $(`#cr_trButton`).append(cr_boxCliqueretire(orderNo, name))
 
-      $("#cr_InfoOpen").click(function (e) {
+      $('#cr_InfoOpen').click(function (e) {
         e.preventDefault()
-         srcModal = "/cliqueretire/information"
-        !$("#cr_dialog").dialog("isOpen") ? $("#cr_dialog").dialog("open") : $("#cr_dialog").dialog("close")
+        srcModal = '/cliqueretire/information'
+        !$('#cr_dialog').dialog('isOpen') ? $('#cr_dialog').dialog('open') : $('#cr_dialog').dialog('close')
       })
 
-      $("#cr_buttonOpen").click(function (e) {
+      $('#cr_buttonOpen').click(function (e) {
         e.preventDefault()
-        srcModal = "/cliqueretire/map"
-        !$("#cr_dialog").dialog("isOpen") ? $("#cr_dialog").dialog("open") : $("#cr_dialog").dialog("close")
+        srcModal = '/cliqueretire/map'
+        !$('#cr_dialog').dialog('isOpen') ? $('#cr_dialog').dialog('open') : $('#cr_dialog').dialog('close')
       })
 
       $(`#cr_cleanerLocker`).click(function (e) {
         e.preventDefault()
         vtexjs.checkout
-          .sendAttachment("shippingData", { address: null, availableAddresses: null, logisticsInfo: null })
+          .sendAttachment('shippingData', { address: null, availableAddresses: null, logisticsInfo: null })
           .then(() => window.location.reload())
           .fail((e) => console.log(e))
-          window.localStorage.removeItem("cr_selectedLocker")
-
-          locker = { userPostalCode: locker.userPostalCode }
+        window.localStorage.removeItem('cr_selectedLocker')
+        locker = { userPostalCode: locker.userPostalCode }
       })
     }
 
-    $("html").append(cr_dialogTemplate);
-
+    $('html').append(cr_dialogTemplate)
     window.onload = () => {
       const Address = window.vtexjs.checkout.orderForm ? window.vtexjs.checkout.orderForm.shippingData.address : locker
-      
-      const FullInfoPayload = AddressInfo(Address, locker);
+      const FullInfoPayload = AddressInfo(Address, locker)
+      const selectSla = logisticInfo()
 
-      if(Address) setItemToStorage(FullInfoPayload)
-      
+      if (Address) setItemToStorage(FullInfoPayload)
+
       var observer = new MutationObserver((mutations) => {
-        if (mutations.length && !$("#cr_trButton").length && $(".shipping-data").length)
+        if (mutations.length && !$('#cr_trButton').length && $('.shipping-data').length)
           mutations.forEach(function () {
-            if ($(".shipping-container").length && !$("#cr_trButton").length)
-              renderLayout(FullInfoPayload ? FullInfoPayload.orderNo : null, FullInfoPayload ? FullInfoPayload.name : null)
+            if ($('.shipping-container').length && !$('#cr_trButton').length)
+              renderLayout(
+                FullInfoPayload ? FullInfoPayload.orderNo : null,
+                FullInfoPayload ? FullInfoPayload.name : null
+              )
           })
       })
 
-      if ($(".shipping-container").length && !$("#cr_trButton").length) 
+      if (
+        ($('.shipping-container').length && !$('#cr_trButton').length && $(carrierSelector).is(':checked') === true) ||
+        selectSla === carrierName
+      )
         renderLayout(FullInfoPayload ? FullInfoPayload.orderNo : null, FullInfoPayload ? FullInfoPayload.name : null)
 
       observer.observe(document.body, { childList: true, attributes: true })
+
+      setInterval(() => {
+        if (window.location.hash == '#/shipping') {
+          const shippingButton = $('#btn-go-to-payment')
+          const hasSelect = JSON.parse(window.localStorage.getItem('cr_selectedLocker'))
+            ? JSON.parse(window.localStorage.getItem('cr_selectedLocker')).name
+            : null
+
+          const selectSla = logisticInfo()
+          if (selectSla === carrierName && !$('#cr_trButton').length) {
+            UpdateCliqueRetire(
+              renderLayout,
+              FullInfoPayload ? FullInfoPayload.orderNo : null,
+              FullInfoPayload ? FullInfoPayload.name : null
+            )
+          }
+
+          hasSelect
+            ? $(carrierSelector).is(':checked') === false
+              ? window.localStorage.removeItem('cr_selectedLocker')
+              : shippingButton.prop('disabled', false)
+            : shippingButton.prop('disabled', $(carrierSelector).is(':checked'))
+        }
+      }, 500)
     }
 
-    $(window).on("deliverySelected.vtex", function () {
+    $(window).on('deliverySelected.vtex', function () {
       const Address = window.vtexjs.checkout.orderForm ? window.vtexjs.checkout.orderForm.shippingData.address : null
-
       const FullInfoPayload = AddressInfo(Address, locker)
-
-      if(Address) setItemToStorage(FullInfoPayload)
-
-      UpdateCliqueRetire(renderLayout, FullInfoPayload ? FullInfoPayload.orderNo : null, FullInfoPayload ? FullInfoPayload.name : null)
+      if (Address) setItemToStorage(FullInfoPayload)
+      UpdateCliqueRetire(
+        renderLayout,
+        FullInfoPayload ? FullInfoPayload.orderNo : null,
+        FullInfoPayload ? FullInfoPayload.name : null
+      )
     })
 
     $(`#cr_dialog`).dialog({
@@ -209,46 +247,46 @@ ${
       resizable: true,
       fluid: true,
       position: {
-        my: "center",
-        at: "center",
+        my: 'center',
+        at: 'center',
         of: window,
       },
-
       open: function () {
-        $(`#cr_dialog`).css("display", "absolute")
-        $(`.step .text input`).css("z-index", "0")
+        $(`#cr_dialog`).css('display', 'absolute')
+        $(`.step .text input`).css('z-index', '0')
         $(`#cr_textDialog`).text(
-          `${srcModal === "/cliqueretire/information" ? "Saiba mais Clique Retire" : "Selecione o melhor local para retirada do seu pedido"}`
+          `${
+            srcModal === '/cliqueretire/information'
+              ? 'Saiba mais Clique Retire'
+              : 'Selecione o melhor local para retirada do seu pedido'
+          }​`
         )
-        $(".payment-data .step").css("z-index", "0")
-        $(".cart-fixed.affix, .cart-fixed.affix-bottom").css("z-index", "0")
-        $(`#cr_iframe`).attr("src", srcModal)
+        $('.payment-data .step').css('z-index', '0')
+        $('.cart-fixed.affix, .cart-fixed.affix-bottom').css('z-index', '0')
+        $(`#cr_iframe`).attr('src', srcModal)
       },
       close: function () {
-        $("#cr_dialog").dialog("close")
+        $('#cr_dialog').dialog('close')
       },
     })
 
-    $(`.ui-dialog-titlebar`).css("display", "none")
-
-    $(`body`).css("max-height", "500px")
-
-    $(`.ui-dialog`).css("margin-top", `100px`)
-
-    $("#cr_closeIframe").click(function () {
-      $("#cr_dialog").dialog("close")
+    $(`.ui-dialog-titlebar`).css('display', 'none')
+    $(`body`).css('max-height', '500px')
+    $(`.ui-dialog`).css('margin-top', `100px`)
+    $('#cr_closeIframe').click(function () {
+      $('#cr_dialog').dialog('close')
     })
   }
 
-  if (typeof jQuery === "undefined" || parseFloat(jQuery.fn.jquery) < 1.7) {
-    loadScript("https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", () => {
-      loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", function () {
+  if (typeof jQuery === 'undefined' || parseFloat(jQuery.fn.jquery) < 1.7) {
+    loadScript('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', () => {
+      loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function () {
         jQuery191 = jQuery.noConflict(true)
         CRCheckout(jQuery191)
       })
     })
   } else {
-    loadScript("https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", () => {
+    loadScript('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', () => {
       CRCheckout(jQuery)
     })
   }
